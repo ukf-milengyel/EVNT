cf36e4bb8f072d54e005efc7b51c73f00088ea1c
UKF-DenisLiptak <61242529+UKF-DenisLiptak@users.noreply.github.com>
2022-10-22 16:53:44 +0200

    Update statistics.blade.php

--- src/resources/views/statistics.blade.php
+++ src/resources/views/statistics.blade.php
@@ -11,16 +11,82 @@
                 <div class="p-6 bg-white border-b border-gray-200">
 
                     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                        Tu bude štatistika
+                        Štatistika
                     </h2>
 
-                    Údaje z controllera<br>
+                    <div class="flex flex-col space-y-8 ...">
+
+                    <div class="grid grid-cols-2 gap-4">
+
+                        <div class="p-6 w-96 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                            <div class="shrink-0">
+                            </div>
+                            <div>
+                                <div class="text-6xl font-medium text-black">{{$attendants}}</div>
+                                <div class="text-3xl font-medium text-black">zúčastnených</div>
+                            </div>
+                        </div>
+
+                        <div class="p-6 w-96 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                            <div class="shrink-0">
+                            </div>
+                            <div>
+                                <div class="text-6xl font-medium text-black">{{$events}}</div>
+                                <div class="text-3xl font-medium text-black">podujatí</div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="grid grid-cols-4 gap-4">
+
+                        <div class="p-6 h-24 w-72 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                            <div class="shrink-0">
+                            </div>
+                            <div>
+                                <div class="text-4xl font-medium text-black">{{$users}}</div>
+                                <div class="text-2xl font-medium text-black">používateľov</div>
+                            </div>
+                        </div>
+
+                        <div class="p-6 h-24 w-72 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                            <div class="shrink-0">
+                            </div>
+                            <div>
+                                <div class="text-4xl font-medium text-black">{{$groups}}</div>
+                                <div class="text-2xl font-medium text-black">skupín</div>
+                            </div>
+                        </div>
+
+                        <div class="p-6 h-24 w-72 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                            <div class="shrink-0">
+                            </div>
+                            <div>
+                                <div class="text-4xl font-medium text-black">{{$photos}}</div>
+                                <div class="text-2xl font-medium text-black">fotografií</div>
+                            </div>
+                        </div>
+
+                        <div class="p-6 h-24 w-72 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                            <div class="shrink-0">
+                            </div>
+                            <div>
+                                <div class="text-4xl font-medium text-black">{{$attachments}}</div>
+                                <div class="text-2xl font-medium text-black">príloh</div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    </div>
+
+                    <!--Údaje z controllera<br>
                     users {{$users}}<br>
                     groups {{$groups}}<br>
                     events {{$events}}<br>
                     attendants {{$attendants}}<br>
                     photos {{$photos}}<br>
-                    attachments {{$attachments}}<br>
+                    attachments {{$attachments}}<br>-->
 
                 </div>
             </div>
