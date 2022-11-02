eac21d7141a01db52ccb34c1748a63f1541aaaa6
miro <miroslav.lengyel@student.ukf.sk>
2022-11-02 21:30:59 +0100

    create event add form, base for event store in controller

--- src/app/Http/Controllers/EventController.php
+++ src/app/Http/Controllers/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -34,7 +35,35 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // todo: authorize, policies
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:2000',
+            'date' => 'required',
+            'organizer' => 'string|max:255',
+            'location_name' => 'string|max:255',
+            'location_address' => 'string|max:255',
+            'image' => 'required|image|mimes:jpg,jpeg,png|max:8192'
+        ]);
+
+        // todo: create compressed version of image with thumbnail before saving
+        $imgname = uniqid('', true) . '.jpg';
+        $request->image->move(public_path('images/event'), $imgname);
+
+        $event = new Event();
+        $event->name = $validated['name'];
+        $event->description = $validated['description'];
+        $event->user_id = $request->user()->id;
+        $event->date = $validated['date'];
+        $event->organizer = $validated['organizer'];
+        $event->location_name = $validated['location_name'];
+        $event->location_address = $validated['location_address'];
+        $event->image = $imgname;
+        $event->save();
+
+        // todo: redirect to event page
+        return view('event.index');
     }
 
     /**
--- src/resources/views/event/add.blade.php
+++ src/resources/views/event/add.blade.php
@@ -7,15 +7,66 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+            <x-std-form>
+                <form method="POST" action="{{ route('event.store') }}" enctype="multipart/form-data">
+                    @csrf
+                    <div>
+                        <x-input-label for="name" value="Názov podujatia" />
+                        <x-std-text-input name="name" type="text" value="{{ old('name') }}" required />
+                    </div>
 
-                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                        Formulár na vytvorenie podujatia
-                    </h2>
+                    <div class="mt-4">
+                        <x-input-label for="description" value="Popis" />
+                        <x-std-textarea name="description" required>{{ old('description') }}</x-std-textarea>
+                    </div>
 
-                </div>
-            </div>
+                    <div class="mt-4">
+                        <x-input-label for="date" value="Dátum" />
+                        <x-std-text-input name="date" type="date" value="{{ old('date') }}" required />
+                    </div>
+
+                    <div class="mt-4">
+                        <x-input-label for="organizer" value="Organizátor (nepovinné)" />
+                        <x-std-text-input name="organizer" type="text" value="{{ old('organizer') }}"/>
+                    </div>
+
+                    <div class="mt-4">
+                        <x-input-label for="location_name" value="Miesto" />
+                        <x-std-text-input name="location_name" type="text" value="{{ old('location_name') }}" required />
+                    </div>
+
+                    <div class="mt-4">
+                        <x-input-label for="location_address" value="Adresa (nepovinné)" />
+                        <x-std-text-input name="location_address" type="text" value="{{ old('location_address') }}"/>
+                    </div>
+
+                    <div class="mt-4">
+                        <x-input-label for="image" value="Fotografia" />
+                        <input id="picker" name="image" type="file" required />
+                        <img id="preview" class="my-2 object-cover relative mx-auto rounded-lg overflow-hidden shadow-lg">
+                    </div>
+
+                    <div class="flex justify-end mt-8">
+                        <x-primary-button>{{ __('Pridať') }}</x-primary-button>
+                    </div>
+                </form>
+            </x-std-form>
+
+            <script>
+                const filePicker = document.getElementById("picker");
+                const imgPreview = document.getElementById("preview");
+
+                filePicker.addEventListener("change", function () {
+                    const files = filePicker.files[0];
+                    if (files) {
+                        const fileReader = new FileReader();
+                        fileReader.readAsDataURL(files);
+                        fileReader.addEventListener("load", function () {
+                            imgPreview.src = this.result;
+                        });
+                    }
+                });
+            </script>
         </div>
     </div>
 </x-app-layout>
