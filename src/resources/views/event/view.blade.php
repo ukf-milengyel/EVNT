bf02c1fef4e076834d68cfe86a428425699d60ab
UKF-MartinValachovič <MartinV0098@gmail.com>
2022-11-05 17:36:57 +0100

    Návrh stránky - Zobrazenie podujatia

--- src/resources/views/event/view.blade.php
+++ src/resources/views/event/view.blade.php
@@ -1,20 +1,51 @@
 <x-barebones>
-    <div class="w-full text-center">
-        <h2 class="font-semibold text-6xl text-gray-800 leading-tight">
-            {{$event->name}}
-        </h2>
+    <div class="mx-5  text-center ">
 
-        desc: {{$event->description}}<br>
-        user: {{$event->user->name}}<br>
+
+        <div class="mx-auto">
+
+            <img src="{{url('/images/event/', $event->image)}}" class="w-auto mx-auto object-cover rounded-lg overflow-hidden shadow-lg">
+
+        </div>
+
+        <div class="flex flex-row">
+            <div class="basis-1/5">
+                <h2 class="font-semibold text-6xl text-gray-800 leading-tight">
+                    {{$event->name}}
+                </h2>
+            </div>
+            <div class="flex-none basis-3/5">
+
+            </div>
+            <div class="basis-1/5 ">
+                <x-primary-button-sm>Share</x-primary-button-sm>
+            </div>
+        </div>
+
+        Description: {{$event->description}}<br>
+        User: {{$event->user->name}}<br>
         @if($event->user->group != NULL)
-            group: {{$event->user->group->name}}<br>
-            color: {{$event->user->group->color}}<br>
+            Group: {{$event->user->group->name}}<br>
+
+                <div class="grid grid-cols-6 gap-5">
+                    <div class="col-start-3 col-end-5">
+            <x-user-badge >
+                <x-slot:group>Color</x-slot:group>
+                <x-slot:color>{{$event->user->group->color}}</x-slot:color>
+            </x-user-badge>
+                    </div>
+                </div>
         @endif
-        date: {{$event->date}}<br>
-        organizer: {{$event->organizer}}<br>
-        location_name: {{$event->location_name}}<br>
-        location_address: {{$event->location_address}}<br>
-        image: {{$event->image}}<br>
-        <img src="{{url('/images/event/', $event->image)}}" class="w-auto mx-auto object-cover rounded-lg overflow-hidden shadow-lg">
+        Date: {{$event->date}}<br>
+        Organizer: {{$event->organizer}}<br>
+        Location Name: {{$event->location_name}}<br>
+        Location Adress: {{$event->location_address}}<br>
+
+
+
+            <div>
+                Prílohy<br>Prílohy<br>Prílohy<br>Prílohy<br>Prílohy<br>Prílohy<br>Prílohy
+            </div>
     </div>
+
 </x-barebones>
