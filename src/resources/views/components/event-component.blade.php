e59918bcd2c57ad057416e168aec0e3b64dbabe7
miro <miroslav.lengyel@student.ukf.sk>
2022-11-05 20:41:55 +0100

    event index polishing

--- src/resources/views/components/event-component.blade.php
+++ src/resources/views/components/event-component.blade.php
@@ -1,19 +1,23 @@
-<div class="p-6 mx-auto rounded-xl shadow-lg items-center space-x-4">
+<div onclick="{{$onclick}}" class="overflow-hidden w-full rounded-xl shadow-lg items-center cursor-pointer">
     <div class="relative">
-        <img class="object-fill h-64 w-96 ..." src="
-                {{($img)}}
-            ">
-        <h3 class="absolute text-xl text-white text-shadow right-2 bottom-2">
-            {{$participants}}
-        </h3>
+        <img class="object-cover h-44 md:h-48 xl:h-52 w-full" src="{{($img)}}">
+        <div class="flex absolute w-full bottom-2 px-2">
+            <div class="truncate w-full flex">
+                {{$organizer}}
+            </div>
+            <div class="flex-1 pl-2 w-auto text-xl text-white text-shadow">
+                {{$participants}}
+            </div>
+
+        </div>
     </div>
-    <div class="text-3xl font-medium text-black">
-        {{$name}}
-    </div>
-    <div class="text-xl font-medium text-black">
-        {{$date}}
-    </div>
-    <div class="text-xl font-medium text-black">
-        {{$organiser}}
+    <div class="px-4 py-2">
+        <div class="text-xl font-medium text-black">
+            {{$name}}
+        </div>
+        <div class="text-md font-medium text-black">
+            {{$date}}
+        </div>
+
     </div>
 </div>
--- src/resources/views/event/index.blade.php
+++ src/resources/views/event/index.blade.php
@@ -54,16 +54,29 @@
 
                     <hr class="my-2">
 
-                    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
 
                         @foreach($events as $event)
 
                             <x-event-component>
                                 <x-slot:img>{{url('/images/event/', $event->image)}}</x-slot:img>
+                                <x-slot:onclick>showDetails('{{route('event.show', $event)}}')</x-slot:onclick>
                                 <x-slot:participants>10</x-slot:participants>
                                 <x-slot:name>{{$event->name}}</x-slot:name>
                                 <x-slot:date>{{$event->date}}</x-slot:date>
-                                <x-slot:organiser>{{$event->organiser}}</x-slot:organiser>
+                                <x-slot:organizer>
+                                    <x-user-badge>
+                                        @if($event->organizer)
+                                            <x-slot:name>{{$event->organizer}}</x-slot:name>
+                                        @else
+                                            <x-slot:name>{{$event->user->name}}</x-slot:name>
+                                        @endif
+                                        @if($event->user->group)
+                                            <x-slot:group>{{$event->user->group->name}}</x-slot:group>
+                                            <x-slot:color>{{$event->user->group->color}}</x-slot:color>
+                                        @endif
+                                    </x-user-badge>
+                                </x-slot:organizer>
                             </x-event-component>
 
                         @endforeach
