7ddd1f18c776c4b1d4c475b780c5a40ef7523052
UKF-MartinValachovič <MartinV0098@gmail.com>
2022-11-19 09:45:01 +0100

    Button - vytvoriť tag

--- src/resources/views/tag/index.blade.php
+++ src/resources/views/tag/index.blade.php
@@ -1,10 +1,21 @@
 <x-app-layout>
     <x-slot name="header">
+        <div class="flex">
+            <div class="flex-auto">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             Tagy
         </h2>
-    </x-slot>
 
+            </div>
+    @can('create', App\Models\Event::class)
+        <div class="justify-end">
+            <a href="{{ route('tag.add') }}" >
+                <x-primary-button-sm>Vytvoriť tag</x-primary-button-sm>
+            </a>
+        </div>
+    @endcan
+        </div>
+    </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
