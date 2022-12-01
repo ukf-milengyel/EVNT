9e34368f2cd1e0852ddfea3290fa6c813e7681ca
UKF-MartinValachovič <MartinV0098@gmail.com>
2022-12-01 16:18:07 +0100

    Footer - 1

--- src/resources/views/tag/index.blade.php
+++ src/resources/views/tag/index.blade.php
@@ -61,3 +61,29 @@
         </div>
     </div>
 </x-app-layout>
+
+<footer class="p-4 bg-white absolute left-0 right-0 bottom-auto shadow md:px-6 md:py-8 dark:bg-gray-900">
+    <div class="sm:flex sm:items-center sm:justify-between">
+        <a href="http://localhost:8080/" class="flex items-center mb-4 sm:mb-0">
+            <x-application-logo class="flex-none h-3 w-3 md:w-10 md:h-10 fill-red-700" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">EVNT</span>
+        </a>
+        <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+                <a href="http://localhost:8080/event" class="mr-4 hover:underline md:mr-6 ">Podujatia</a>
+            </li>
+            <li>
+                <a href="http://localhost:8080/announcement" class="mr-4 hover:underline md:mr-6">Oznámenia</a>
+            </li>
+            <li>
+                <a href="http://localhost:8080/tag" class="mr-4 hover:underline md:mr-6 ">Tagy</a>
+            </li>
+            <li>
+                <a href="http://localhost:8080/statistics" class="hover:underline">Štatistika</a>
+            </li>
+        </ul>
+    </div>
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="http://localhost:8080/" class="hover:underline">EVNT</a>. All Rights Reserved.
+    </span>
+</footer>
