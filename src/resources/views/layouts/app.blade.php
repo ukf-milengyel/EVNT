d909a3604f104b0aacbd73382bd15d86accb5a5d
UKF-MartinValachovič <MartinV0098@gmail.com>
2022-12-01 16:59:54 +0100

    Footer - 3

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -36,12 +36,17 @@
             </main>
         </div>
 
-        <footer class="p-4 bg-white absolute left-0 right-0 bottom-auto shadow md:px-6 md:py-8 dark:bg-gray-900">
+        <footer class=" bg-white absolute left-0 right-0 bottom-auto shadow md:px-6 md:py-3 dark:bg-gray-800">
             <div class="sm:flex sm:items-center sm:justify-between">
-                <a href="http://localhost:8080/" class="flex items-center mb-4 sm:mb-0">
-                    <x-application-logo class="flex-none h-3 w-3 md:w-10 md:h-10 fill-red-700" />
+                <a href="http://localhost:8080/" class="flex items-center mb-4 sm:mb-4">
+                    <x-application-logo class="flex-none w-10 h-10 md:w-10 md:h-10 fill-red-700" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">EVNT</span>
                 </a>
+
+                <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                © 2022 <a href="http://localhost:8080/" class="hover:underline">EVNT</a>. All Rights Reserved.
+                </span>
+
                 <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
                     <li>
                         <a href="http://localhost:8080/event" class="mr-4 hover:underline md:mr-6 ">Podujatia</a>
@@ -56,10 +61,8 @@
                         <a href="http://localhost:8080/statistics" class="hover:underline">Štatistika</a>
                     </li>
                 </ul>
+
             </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="http://localhost:8080/" class="hover:underline">EVNT</a>. All Rights Reserved.
-    </span>
         </footer>
     </body>
 </html>
