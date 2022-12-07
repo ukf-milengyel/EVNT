871cb42b99ff82ed89bc7fe6b2491980177ccddf
ukf-milengyel <114596047+ukf-milengyel@users.noreply.github.com>
2022-12-07 13:31:40 +0100

    more tag index nonsense

--- src/resources/views/tag/index.blade.php
+++ src/resources/views/tag/index.blade.php
@@ -27,38 +27,40 @@
         </x-std-alert>
     @endif
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-5 w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+    @if(count($tags) != 0)
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-5 w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-4">
 
-                    @foreach($tags as $tag)
-                        <div class="flex w-full h-12 md:h-7 px-3 md:px-1 rounded-3xl md:rounded-xl border-2 flex items-center space-x-1">
-                            <div class="flex-initial">
-                                <x-tag-view>
-                                    {{$tag->name}}
-                                </x-tag-view>
-                            </div>
-                            <div class="flex-auto">
-                            </div>
-                            <div class="flex-none">
-                                <a href="{{route('tag.edit', $tag)}}">
-                                <img src="{{asset('/icons/edit.svg')}}" class="inline w-8 md:w-4 my-auto cursor-pointer">
-                                </a>
+                        @foreach($tags as $tag)
+                            <div class="flex w-full h-12 md:h-7 px-3 md:px-1 rounded-3xl md:rounded-xl border-2 flex items-center space-x-1">
+                                <div class="flex-initial">
+                                    <x-tag-view>
+                                        {{$tag->name}}
+                                    </x-tag-view>
+                                </div>
+                                <div class="flex-auto">
+                                </div>
+                                <div class="flex-none">
+                                    <a href="{{route('tag.edit', $tag)}}">
+                                        <img src="{{asset('/icons/edit.svg')}}" class="inline w-8 md:w-4 my-auto cursor-pointer">
+                                    </a>
 
-                                <form class="inline" method="POST" action="{{ route('tag.destroy', $tag) }}">
-                                    @csrf
-                                    @method('delete')
-                                <img src="{{asset('/icons/delete.svg')}}" class="inline w-8 md:w-4 my-auto cursor-pointer" :href="route('tag.destroy', $tag)" onclick="event.preventDefault(); if(confirm('Chcete odstrániť tento tag?')){this.closest('form').submit();}">
-                                </form>
+                                    <form class="inline" method="POST" action="{{ route('tag.destroy', $tag) }}">
+                                        @csrf
+                                        @method('delete')
+                                        <img src="{{asset('/icons/delete.svg')}}" class="inline w-8 md:w-4 my-auto cursor-pointer" :href="route('tag.destroy', $tag)" onclick="event.preventDefault(); if(confirm('Chcete odstrániť tento tag?')){this.closest('form').submit();}">
+                                    </form>
+                                </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
 
+
+                    </div>
 
                 </div>
-
             </div>
-        </div>
+    @endif
     </div>
-</x-app-layout>
+    </x-app-layout>
 
